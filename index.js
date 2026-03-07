c9ab2086b16d62e0b14998c357983f5bdcaf6370
AADI SRIPAD <sripadadhi135@gmail.com>
2026-03-07 09:54:42 +0530

    added store section

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-import express from 'express'
-import expressLayouts from 'express-ejs-layouts'
-import session from 'express-session'
-import {productRouter} from './routes/productRoute.js'
-import {userRouter} from './routes/userRoute.js'
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+import session from "express-session";
+// import {productRouter} from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
+const app = express();
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const app = express()
-app.set('view engine','ejs')
-app.set('views','views')
-app.use(expressLayouts)
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(
+  session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
-app.use(session({
-    secret:'mySecretKey',
-    resave:false,
-    saveUninitialized:false,
-}))
+app.use("/", storeRouter);
+// app.use("/auth", authRouter);
+// app.use("/products", productRouter);
+// app.use("/users", userRouter);
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-app.use('/products',productRouter)
-app.use('/users',userRouter)
-
-app.listen(5000,()=>{
-    console.log('Server Started');
-})
+app.listen(5000, () => {
+  console.log("Server Started");
+});
