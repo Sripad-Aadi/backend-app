78e53bc317a49ffd67a95035be5aa531230081d3
AADI SRIPAD <sripadadhi135@gmail.com>
2026-03-07 15:36:41 +0530

    connected mongodb server

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 // import {productRouter} from "./routes/productRoute.js";
+import dbConnect from './config/db.js'
 import { storeRouter } from "./routes/storeRoute.js";
+
 const app = express();
+dotenv.config()
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('layout','layout')
 
 app.use(
   session({
@@ -23,6 +29,11 @@ app.use("/", storeRouter);
 // app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
-app.listen(5000, () => {
-  console.log("Server Started");
-});
+const startServer = async ()=>{
+  await dbConnect();
+  app.listen(5000, () => {
+    console.log("Server Started");
+  });
+}
+
+startServer()
--- routes/productRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { showProducts } from "../controllers/productController";
-
-const productRouter = express.Router()
-
-productRouter.get('/',showProducts)
-
-export {productRouter}
