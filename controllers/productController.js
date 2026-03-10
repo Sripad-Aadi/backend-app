353d5816d35073950412d6b8ec02c49be8fbd97d
AADI SRIPAD <sripadadhi135@gmail.com>
2026-03-10 12:37:03 +0530

    implemented all crud oper for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,35 @@
 import productModel from "../models/productModel.js";
 
 const getProducts = async (req,res)=>{
-    const products =await productModel.find()
+    const products = await productModel.find()
     res.render('products/index',{products})
 }
 
-export {getProducts}
+const addProductForm = async (req, res)=>{
+    res.render('products/add')
+}
+
+const addProduct = async (req, res)=>{
+    const product = req.body
+    await productModel.create(product)
+    res.redirect('/products')
+}
+
+const editProductForm = async (req,res)=>{
+    const id = req.params.id;
+    const product = await productModel.findOne({_id:id})
+    res.render('products/edit',{product})
+}
+
+const editProduct = async (req, res)=>{
+    const id = req.params.id
+    await productModel.findByIdAndUpdate(id,req.body)
+    res.redirect('/products')
+}
+const deleteProduct = async (req, res)=>{
+    const id = req.params.id
+    await productModel.findByIdAndDelete(id)
+    res.redirect('/products')
+}
+
+export {getProducts, addProduct, addProductForm, editProductForm, editProduct, deleteProduct};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { getProducts } from "../controllers/productController.js"
+import { getProducts, addProductForm, addProduct, editProductForm, deleteProduct, editProduct} from "../controllers/productController.js"
 
 const productRouter = express.Router()
 
 productRouter.get("/",getProducts)
+productRouter.get('/add',addProductForm)
+productRouter.post('/add',addProduct)
+productRouter.get('/:id/delete',deleteProduct)
+productRouter.get('/:id/edit',editProductForm)
+productRouter.post('/:id/save',editProduct)
 
 export default productRouter
