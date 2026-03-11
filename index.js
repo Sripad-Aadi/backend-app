119b94ee0fc521cc5abac6f15043fb359e6ecb5f
AADI SRIPAD <sripadadhi135@gmail.com>
2026-03-11 13:49:08 +0530

    updated

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import session from "express-session";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import { authenticateAdmin } from "./middleware/auth.js";
 import productRouter from "./routes/productRoute.js";
-import userRouter from './routes/userRoute.js'
+import homeRouter from "./routes/homeRoute.js";
+import authRouter from "./routes/authRoute.js";
+import userRouter from "./routes/userRoute.js";
 import dbConnect from './config/db.js'
 import storeRouter from "./routes/storeRoute.js";
 
@@ -28,10 +31,11 @@ app.use(
   }),
 );
 
-app.use("/", storeRouter);
-// app.use("/auth", authRouter);
-app.use("/products", productRouter);
-app.use("/users", userRouter);
+//app.use("/auth", authRouter);
+app.use("/store", storeRouter);
+app.use("/", authenticateAdmin, homeRouter);
+//app.use("/products",authenticateAdmin, productRouter);
+//app.use("/users",authenticateAdmin, userRouter);
 
 const startServer = async ()=>{
   await dbConnect();
