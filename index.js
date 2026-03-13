0ff003c8901d92526177912ad1e5116827d255ce
AADI SRIPAD <sripadadhi135@gmail.com>
2026-03-13 11:01:57 +0530

    handled the dynamic port allotment for render deploy failure

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ app.use("/", authenticateAdmin, homeRouter);
 app.use("/products",authenticateAdmin, productRouter);
 app.use("/users",authenticateAdmin, userRouter);
 
+const PORT = process.env.PORT || 5000;
+
 const startServer = async ()=>{
   await dbConnect();
-  app.listen(5000, () => {
+  app.listen(PORT, () => {
     console.log("Server Started");
   });
 }
